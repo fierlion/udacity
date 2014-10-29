730841f255e273a535adb1715e92dd16938fda23
rfoote <fierlion@gmail.com>
2014-10-29 11:20:47 -0700

    itertools permutations syntax add function 'nextTo()'

--- iterToolTest.py
+++ iterToolTest.py
@@ -3,6 +3,10 @@ import itertools
 houses = [1,2,3]
 orderings = list(itertools.permutations(houses))
 
+def nextTo(ob1, ob2):
+    "Object ob1 is immediately next to ob2"
+    return abs(h1 - h2) == 1
+
 for (red, green, blue) in orderings:
     for (hot, hotter, hottest) in orderings:
         #print (red == hottest) #'==' checks for same value
