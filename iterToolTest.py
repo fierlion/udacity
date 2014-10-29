757a72bf7c9148dd1248dbc55bb4a2132ff42584
rfoote <fierlion@gmail.com>
2014-10-29 11:14:48 -0700

    itertools permutations syntax add 'is'

--- iterToolTest.py
+++ iterToolTest.py
@@ -5,4 +5,5 @@ orderings = list(itertools.permutations(houses))
 
 for (red, green, blue) in orderings:
     for (hot, hotter, hottest) in orderings:
-        print (red == hottest)
+        #print (red == hottest) #'==' checks for same value
+        print (red is hottest) # checks for same Object
